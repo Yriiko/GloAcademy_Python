71bf6acdcdae2174ad777f8df8af4d25307409e8
Yrii <yrii.kostitsin@gmail.com>
2021-06-13 14:49:24 +0300

    Выполнил задания к уроку 3

---
+++ Урок 3/lesson_3-1.py
@@ -0,0 +1,8 @@
+# Задание 1
+# Вариант 1
+print('Glo Academy')
+print()
+print('Glo Academy')
+
+# Вариант 2
+print('Glo Academy', '', 'Glo Academy', sep='\n')
---
+++ Урок 3/lesson_3-3.py
@@ -0,0 +1,8 @@
+# Задание 3
+print('Как вас зовут?')
+name = input()
+print('Как ваша фамилия?')
+surname = input()
+print('Как ваше отчество?')
+middle_name = input()
+print(name, surname, middle_name, sep='-')
