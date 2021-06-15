88c6c3142b012b64b84d53f6f48d8cc9223eacd4
Yrii <yrii.kostitsin@gmail.com>
2021-06-16 00:15:28 +0300

    Выполнил задания к уроку 5. Исправил задание 6

--- Урок 5/lesson_5-6.py
+++ Урок 5/lesson_5-6.py
@@ -2,14 +2,23 @@
 print('Введите номер кармана')
 n = int(input())
 
-
-if (n >= 1 and n <= 10 and n >= 19 and n <= 28 and n % 2 == 0) or (n >= 11 and n <= 18 and n >= 29 and n <= 36 and n % 2 != 0):
-    print('черный')
-else:
-    print('красный')
-
 if n == 0:
     print('зеленый')
 
-if n > 36:
+elif n % 2 == 0 and 1 <= n <= 10:
+    print('черный')
+
+elif n % 2 == 0 and 19 <= n <= 28:
+    print('черный')
+
+elif n % 2 != 0 and 11 <= n <= 18:
+    print('черный')
+
+elif n % 2 != 0 and 29 <= n <= 36:
+    print('черный')
+
+elif n > 36:
     print('ошибка ввода')
+
+else:
+    print('красный')
